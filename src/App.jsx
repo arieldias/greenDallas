3c4b724d99d982da602b78b40850013e1e79f6ca
Ariel Dias <ariel.dias@objectedge.com>
2019-01-23 09:17:58 -0200

    Exploring environment

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import React, {Component} from 'react';
+import Buttons from './Buttons.jsx';
 
 class App extends Component {
   render() {
+    
     return (
-      <h1>Hello React :)</h1>
+      <div>
+        <h1> this is a test  2 </h1>
+        <Buttons text="Send"/>
+      </div>
     );
   }
 }
