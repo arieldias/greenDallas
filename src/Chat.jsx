54b05835857eb30e083048cdb26b6c0a1728a5ef
Ariel Dias <ariel.dias@objectedge.com>
2019-01-25 09:01:27 -0200

    Adding orange pic, refactoring newEntry, creating resposiveness

--- src/Chat.jsx
+++ src/Chat.jsx
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import {Line} from 'react-chartjs-2';
 
 
+
 class Chart extends Component {  
   constructor(props) {
     super(props);
@@ -15,6 +16,7 @@ class Chart extends Component {
     this.handleChange = this.handleChange.bind(this);
     this.sendMessage  = this.sendMessage.bind(this);
     this.addMessage   = this.addMessage.bind(this);
+    this.renderImage  = this.renderImage.bind(this);
     
   }  
   componentDidMount() {
@@ -30,13 +32,25 @@ class Chart extends Component {
 		})
 			.catch(err => console.error(this.props.url, err.toString()));
   }
+  renderImage (portrait) {
+    if (portrait) {
+      return(
+        <img src={portrait} />
+      )
+    } else {
+      return (
+        <div className="chat__element__image__noPortrait"></div>
+      )
+    }
+  }
 
   getStructuredMessage (item) {
     if (item.displayPortraitLeft) {
       return (
         <li className="chat__element row">
           <div className="chat__element__image col-sm-2">
-             <img src={item.portrait} />
+            {this.renderImage(item.portrait) }
+            
           </div>
           <div className="chat__element__info col-sm-10">
               <div className="chat__element__info__meta">
@@ -82,40 +96,32 @@ class Chart extends Component {
     this.sendMessage();  
   }
 
-  addMessage () {
-    var newEntry = {
-      userName: "Eu",
-      portrait: "",
-      displayPortraitLeft: true,
-      time: "1 minute ago",
-      message : this.state.newMessage
-    }
-   
+  addMessage (newEntry) {
     var updatedMessages = this.state.messages;
     updatedMessages[updatedMessages.length] = newEntry;
 
    this.setState({messages : updatedMessages});
-   
-   
   }
 
   sendMessage() {
+    var newEntry = {
+      userName: 'Eu',
+      time: '1 min ago',
+      message: this.state.newMessage,
+      portrait: null,
+      displayPortraitLeft: true
+    }
+
     fetch('http://dev.4all.com:3050/messages', {
       method: 'POST',
       headers: {
         'Accept': 'application/json',
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({
-        userName: 'Eu',
-        time: '1 min ago',
-        message: this.state.newMessage,
-        portrait: null,
-        displayPortraitLeft: true
-      })
+      body: JSON.stringify(newEntry)
     }).then(res => {
       console.log(res);
-      this.addMessage();
+      this.addMessage(newEntry);
     })
   }
 
@@ -133,8 +139,7 @@ class Chart extends Component {
               
                 return (
                   __this.getStructuredMessage(item)
-                )
-          
+                )          
               })
             } 
           </ul>
--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -12,41 +12,41 @@ class NavBar extends Component {
 
     return (
       <div className="nav row">
-        <div className="nav__element col-sm-3" > 
-          <div className="nav__element__icon col-sm-4 nav__element__icon--blue">
+        <div className="nav__element col-xs-12 col-md-6 col-lg-3" > 
+          <div className="nav__element__icon col-xs-12 col-sm-4 nav__element__icon--blue">
             <i className="fas fa-shopping-bag "></i>
           </div>
-          <div className="nav__element__info col-sm-8">
+          <div className="nav__element__info col-xs-12 col-sm-8">
             <span className="nav__element__info__number"> {totalNewOrders}</span>
             <span className="nav__element__info__name"> New Orders </span>
           </div>
         </div>
 
-        <div className="nav__element col-sm-3"> 
-          <div className="nav__element__icon col-sm-4 nav__element__icon--yellow">
+        <div className="nav__element col-xs-12 col-md-6 col-lg-3"> 
+          <div className="nav__element__icon col-xs-12 col-sm-4 nav__element__icon--yellow">
             <i className="far fa-comment"></i>
           </div>
-          <div className="nav__element__info col-sm-8">
+          <div className="nav__element__info col-xs-12 col-sm-8">
             <span className="nav__element__info__number"> {totalComments} </span>
             <span className="nav__element__info__name"> Comments </span>
           </div>
         </div>
 
-        <div className="nav__element col-sm-3"> 
-          <div className="nav__element__icon col-sm-4 nav__element__icon--green">
+        <div className="nav__element col-xs-12 col-md-6 col-lg-3"> 
+          <div className="nav__element__icon col-xs-12 col-sm-4 nav__element__icon--green">
             <i className="far fa-user"></i>
           </div>
-          <div className="nav__element__info col-sm-8">
+          <div className="nav__element__info col-xs-12 col-sm-8">
             <span className="nav__element__info__number">{totalNewUsers} </span>
             <span className="nav__element__info__name"> New Users </span>
           </div>
         </div>
 
-        <div className="nav__element col-sm-3"> 
-          <div className="nav__element__icon col-sm-4  nav__element__icon--red">
+        <div className="nav__element col-xs-12 col-md-6 col-lg-3"> 
+          <div className="nav__element__icon col-xs-12 col-sm-4  nav__element__icon--red">
             <i className="far fa-newspaper"></i>
           </div>
-          <div className="nav__element__info col-sm-8">
+          <div className="nav__element__info col-xs-12 col-sm-8">
             <span className="nav__element__info__number"> {totalPageViews > 1000 ? (totalPageViews / 1000).toFixed(2) + "K" : totalPageViews } </span>
             <span className="nav__element__info__name"> Page Views </span>
           </div>
